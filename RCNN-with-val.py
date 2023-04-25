5bb11bdb37484d2ec2dee7b2b7ee32418ac13b93
JoshuaUhlfelder <jfu2001@columbia.edu>
2023-04-25 17:24:28 -0400

    Edited lr and optimizer

--- RCNN-with-val.py
+++ RCNN-with-val.py
@@ -132,6 +132,98 @@ def loadData():
     return batch_Imgs, batch_Data
 
  
+
+
+
+
+
+def loadValData():
+    batch_Imgs=[]
+    batch_Data=[]
+    window_Data=[]
+    #First tablet is validation data
+    idx=0
+    #Get the names of all the tifs in the selected folder
+    layer_names = os.listdir(img_collections[idx] + 'surface_volume')
+    
+    #Validation data is always max_image_size x max_image_size
+    window_size = math.floor(max_image_size/2)
+    
+    #Get the center of the window on the image
+    big_mask = rasterio.open(img_collections[idx] + 'mask.png')
+    height = big_mask.height
+    width = big_mask.width
+    window_center_width = 0
+    window_center_height = 0
+    
+    window_center_width = random.randint(window_size,width - window_size - 1)
+    window_center_height = random.randint(window_size,height - window_size - 1)
+
+    big_mask.close()
+    #Set the window bounds
+    window = Window(window_center_width - window_size, 
+                    window_center_height - window_size, window_size*2, window_size*2)
+
+    #for each TIF, open the file in the window and add to the tensor
+    full_img = np.zeros((max_image_size,max_image_size,65))
+    layer_names.sort()
+    for j in range(65):
+        with rasterio.open(img_collections[idx] + 'surface_volume/' + layer_names[j]) as img:
+            chunk = img.read(1, window=window)
+            chunk = cv2.resize(chunk, [max_image_size,max_image_size], cv2.INTER_LINEAR)
+            full_img[:,:,j] = chunk
+
+    #Now window and resize the mask and create a new mask for each glob
+    masks=[]
+    mask_file=os.path.join(img_collections[idx], 'inklabels.png')
+    with rasterio.open(mask_file) as mk:
+        vesMask = mk.read(1, window=window)
+        vesMask = (vesMask > 0).astype(np.uint8) 
+        #Resize the image
+        vesMask=cv2.resize(vesMask,[max_image_size,max_image_size],cv2.INTER_NEAREST)
+        #Find the connected regions
+        (numLabels, labels, stats, centroids) = cv2.connectedComponentsWithStats(vesMask, connectivity=8)
+        #Create a new mask for each connected region
+        for i in range(1, numLabels):
+            new_mask = np.where(labels == i, 1, 0)
+            new_mask = (new_mask > 0).astype(np.uint8) 
+            masks.append(new_mask)
+        
+        num_objs = len(masks)
+        if num_objs==0: 
+            return loadValData()
+
+        boxes = torch.zeros([num_objs,4], dtype=torch.float32)
+        for i in range(num_objs):
+            x,y,w,h = cv2.boundingRect(masks[i])
+            boxes[i] = torch.tensor([x, y, x+w, y+h])
+        masks = np.array(masks)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+        full_img = torch.as_tensor(full_img, dtype=torch.float32)
+        data = {}
+        data["boxes"] =  boxes
+        data["labels"] =  torch.ones((num_objs,), dtype=torch.int64)   
+        data["masks"] = masks
+        batch_Imgs.append(full_img)
+        batch_Data.append(data)  
+        window_Data.append(window)
+
+    batch_Imgs=torch.stack([torch.as_tensor(d) for d in batch_Imgs],0)
+    batch_Imgs = batch_Imgs.swapaxes(1, 3).swapaxes(2, 3)
+    print(window_Data)
+    return [batch_Imgs, batch_Data, window_Data]
+
+
+
+
+
+
+
+
+
+
+
+
 #Load in the Mask RCNN model
 model=torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True, 
                                                          image_mean = torch.tensor(np.full(65,0.5)),
@@ -150,9 +242,10 @@ model.to(device)
 print(device)
 
 #Set optimizer and lr
-lr = 3e-5
+lr = 3e-4
 print("Set learning rate to:", lr)
-optimizer = torch.optim.AdamW(params=model.parameters(), lr=lr)
+optimizer = torch.optim.Adam(params=model.parameters(), lr=lr)
+criterion = nn.CrossEntropyLoss()
 
 
 
@@ -161,12 +254,18 @@ optimizer = torch.optim.AdamW(params=model.parameters(), lr=lr)
 #Then output the F0.5 score
 def score(true_mask, predicted):
     
+    unique, counts = np.unique(predicted, return_counts=True)
+    print("predicted counts:", dict(zip(unique, counts)))
+    
+    unique, counts = np.unique(true_mask, return_counts=True)
+    print("true counts:", dict(zip(unique, counts)))
+    
     TP = np.sum(np.logical_and(predicted == 1, true_mask == 1))
     TN = np.sum(np.logical_and(predicted == 0, true_mask == 0))
     FP = np.sum(np.logical_and(predicted == 1, true_mask == 0))
     FN = np.sum(np.logical_and(predicted == 0, true_mask == 1))
     
-    print('TP: %i, FP: %i, TN: %i, FN: %i' % (TP,FP,TN,FN))
+    #print('TP: %i, FP: %i, TN: %i, FN: %i' % (TP,FP,TN,FN))
     
     p = TP/(TP+FP)
     r = TP/(TP+FN)
@@ -200,81 +299,60 @@ for i in range(401):
         
         model.eval()
 
-        m = 0
-        print("testing on fragment", img_collections[m])
-        ####Evaulation loop
-        #Create the big mask with the same dimensions as the og. files
-        big_mask = rasterio.open(img_collections[m] + 'inklabels.png')
-        height = big_mask.height
-        width = big_mask.width
-        window = Window(0,0,width,height)
-        true_mask = big_mask.read(1, window=window)
-        true_mask = (true_mask > 0).astype(np.uint8)
-        #Ensure the selected center of window is within the fragment
-        big_mask.close()
+        m = 0 #Only first fragement used for validation
+
+        print("testing 10 samples on fragment", img_collections[m])
+
+        val_loops = 10
         
-        combined = np.zeros((height, width))
-        
-        for l in [600]:
-            max_image_size= l
-            
-            #Get the number of full blocks in the x and y directions
-            #This helps us divide up the image
-            x_range = math.ceil(width/max_image_size)
-            y_range = math.ceil(height/max_image_size)
+        all_losses = []
+        f_5_scores = []
+        for l in range(val_loops):
             
             #create an empty matrix for our mask we'll make
-            result = np.zeros((height, width))
+            print("Validation", l)
+
+            [images, targets, windows] = loadValData()
+            window = windows[0]
+            images = list(image.to(device) for image in images)
+            targets=[{k: v.to(device) for k,v in t.items()} for t in targets]
             
-            #Divide the image into chunks
-            for x in range(x_range):
-                for y in range(y_range):
-                    coords = [y*max_image_size,x*max_image_size]
-                    #If the image goes beyond the bounds, reset the coords back
-                    if coords[0] + max_image_size >= height:
-                        coords[0] = height-max_image_size
-                    if coords[1] + max_image_size >= width:
-                        coords[1] = width-max_image_size
-                        
-                    #for each chunk, make a window and load in the image
-                    window = Window(coords[1], coords[0], max_image_size, max_image_size)
-                    print(window)
-                    images = np.zeros((max_image_size,max_image_size,65))
-                    layer_names = os.listdir(img_collections[m] + 'surface_volume')
-                    layer_names.sort()
-                    #Add each layer of the fragment to the stack of images
-                    for j in range(65):
-                        with rasterio.open(img_collections[m] + 'surface_volume/' + layer_names[j]) as img:
-                            chunk = img.read(1, window=window)
-                            chunk = cv2.resize(chunk, [max_image_size,max_image_size], cv2.INTER_LINEAR)
-                            images[:,:,j] = chunk
-                    #Add the chunk to the image file
-                    #Track the coordinates of the image so we can reassemble the large picture
+            
+            with torch.no_grad():
+                pred = model(images)
+                model.train()
+                loss_dict = model(images, targets)
+            this_loss = sum(loss for loss in loss_dict.values())
+            model.eval()
+                
+
+                
+            #Get the true mask
+            mask_file=os.path.join(img_collections[m], 'inklabels.png')
+            true_mask = None
+            with rasterio.open(mask_file) as mk:
+                true_mask = mk.read(1, window=window)
+                true_mask = (true_mask > 0).astype(np.uint8) 
+                #Resize the image
+                true_mask=cv2.resize(true_mask,[max_image_size,max_image_size],cv2.INTER_NEAREST)
+            
+            im = np.zeros((max_image_size,max_image_size))
+            for k in range(len(pred[0]['masks'])):
+                msk=pred[0]['masks'][k,0].detach().cpu().numpy()
+                scr=pred[0]['scores'][k].detach().cpu().numpy()
+                if scr>0.6 :
+                    im[:,:][msk>0.5] = 1
+            
+            f_5_scores.append(score(true_mask, im))
+            all_losses.append(this_loss)
+            
+            
+        print('Average F0.5 score:', sum(f_5_scores)/val_loops)
+        print(f_5_scores)
+        print('Average loss:', sum(all_losses)/val_loops)
+        print(all_losses)
         
-                    images = torch.as_tensor(images, dtype=torch.float32).unsqueeze(0)
-                    images=images.swapaxes(1, 3).swapaxes(2, 3)
-                    images = list(image.to(device) for image in images)
-            
-                    with torch.no_grad():
-                        pred = model(images)
-                    
-                    im = np.zeros((max_image_size,max_image_size))
-                    for k in range(len(pred[0]['masks'])):
-                        msk=pred[0]['masks'][k,0].detach().cpu().numpy()
-                        msk = cv2.resize(msk, [max_image_size,max_image_size], cv2.INTER_NEAREST)
-                        scr=pred[0]['scores'][k].detach().cpu().numpy()
-                        if scr>0.6 :
-                            im[:,:][msk>0.5] = 1
-                    result[coords[0]:(coords[0]+max_image_size), 
-                           coords[1]:(coords[1]+max_image_size)] = im
-                        
-            combined = np.add(combined, result)
-            
-            
-        final = np.where(combined>0, 1, 0)
         
-        f_5_score = score(true_mask, final)
-        print('F0.5 score:', f_5_score)
         
         
         
